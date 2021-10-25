0343dbca9ff90c7ab9988bbf3b52d3f8340ce25d
Lukas Tobler <lutobler@student.ethz.ch>
2021-10-25 12:05:27 +0200

    Update cuda-traces with analysis

---
+++ cuda-traces/analysis.md
@@ -0,0 +1,7 @@
+Application | Kernel launches | Unique Kernels | cudaMalloc | Copy H2D | Copy D2D | Copy D2H
+---
+hotspot.log | 25 | 1 | 3 | 2 | 0 | 1 
+dwt2d.log | 10 | 4 | 12 | 1 | 9 | 3 
+srad2.log | 4 | 2 | 6 | 2 | 0 | 2 
+resnet50.log | 310 | 21 | 37 | 321 | 3 | 3 
+bert-squad.log | 714 | 22 | 74 | 397 | 1 | 3 
---
+++ cuda-traces/analysis.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+out=analysis.md
+files=(
+    hotspot.log
+    dwt2d.log
+    srad2.log
+    resnet50.log
+    bert-squad.log
+)
+
+printf 'Application | Kernel launches | Unique Kernels | cudaMalloc | Copy H2D | Copy D2D | Copy D2H\n' > "$out"
+/usr/bin/printf '---\n' >> "$out"
+for f in ${files[@]}; do
+    n_kernel=$(grep 'cuLaunchKernel' "$f" | wc -l)
+    n_malloc=$(grep 'cudaMalloc' "$f" | wc -l)
+    n_cpyd2h=$(grep 'DeviceToHost' "$f" | wc -l)
+    n_cpyh2d=$(grep 'HostToDevice' "$f" | wc -l)
+    n_cpyd2d=$(grep 'DeviceToDevice' "$f" | wc -l)
+    n_uniq_kernel=$(grep -E 'cuLaunchKernel' "$f" | sed 's/^\(cuLaunchKernel(.*\)\ \[.*$/\1/' | sort | uniq | wc -l)
+
+    printf "$f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2d | $n_cpyd2h \n" >> "$out"
+done
