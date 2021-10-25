d162749bcd9da4bd681149acf9578dc04a5203ff
Lukas Tobler <lutobler@student.ethz.ch>
2021-10-25 12:11:56 +0200

    Fix markdown for cuda trace analysis

--- cuda-traces/analysis.sh
+++ cuda-traces/analysis.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-out=analysis.md
 files=(
     hotspot.log
     dwt2d.log
@@ -9,8 +8,8 @@ files=(
     bert-squad.log
 )
 
-printf 'Application | Kernel launches | Unique Kernels | cudaMalloc | Copy H2D | Copy D2D | Copy D2H\n' > "$out"
-/usr/bin/printf '---\n' >> "$out"
+printf '| Application | Kernel launches | Unique Kernels | cudaMalloc | Copy H2D | Copy D2D | Copy D2H |\n'
+/usr/bin/printf '| --- | --- | --- | --- | --- | --- | --- |\n'
 for f in ${files[@]}; do
     n_kernel=$(grep 'cuLaunchKernel' "$f" | wc -l)
     n_malloc=$(grep 'cudaMalloc' "$f" | wc -l)
@@ -19,5 +18,5 @@ for f in ${files[@]}; do
     n_cpyd2d=$(grep 'DeviceToDevice' "$f" | wc -l)
     n_uniq_kernel=$(grep -E 'cuLaunchKernel' "$f" | sed 's/^\(cuLaunchKernel(.*\)\ \[.*$/\1/' | sort | uniq | wc -l)
 
-    printf "$f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2d | $n_cpyd2h \n" >> "$out"
+    printf "| $f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2d | $n_cpyd2h |\n"
 done
