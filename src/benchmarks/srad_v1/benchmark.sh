c8a0db689394d9ce86398e9dacb82a38b18bdb37
Lukas Tobler <lutobler@student.ethz.ch>
2021-11-20 21:32:55 +0100

    Update benchmark script for srad_v1

--- src/benchmarks/srad_v1/benchmark.sh
+++ src/benchmarks/srad_v1/benchmark.sh
@@ -6,18 +6,38 @@ project_dir=../../../
 manager_bin=$project_dir/src/build/manager_trace
 bench_dir=$project_dir/src/benchmarks/srad_v1
 
-echo 'local native performance'
-pushd .
-cd $bench_dir
-printf '' > $out_file-local.out
-for i in {1..100}; do
-    echo run $i
-    t=$(./run | tail -n1 | cut -d' ' -f1)
-    printf "$t\n"
-    printf "$t\n" >> "${out_file}-local.out"
-done
-popd
+if [[ "$1" == "local" ]]; then
+    echo 'local native performance'
+    pushd .
+    cd $bench_dir
+    printf '' > $out_file-local.out
+    for i in {1..100}; do
+        echo run $i
+        t=$(./run | tail -n1 | cut -d' ' -f1)
+        printf "$t\n"
+        printf "$t\n" >> "${out_file}-local.out"
+    done
+    popd
+elif [[ "$1" == "local-net" ]]; then
+    echo 'local network performance'
+    pushd .
+    cd $bench_dir
+    printf '' > $out_file-local-network.out
+    for i in {1..100}; do
+        echo run $i
+        SPDLOG_LEVEL=off $manager_bin &
+        sleep 0.5
+        t=$(SPDLOG_LEVEL=off CUDA_BINARY=./srad EXECUTOR_TYPE=tcp LD_PRELOAD=$project_dir/src/build/libgpuless.so ./run | tail -n1 | cut -d' ' -f1)
+        printf "$t\n"
+        printf "$t\n" >> "${out_file}-local-network.out"
+        pkill manager_trace
+        sleep 0.5
+    done
+    popd
+elif [[ "$1" == "remote-net" ]]; then
+    echo TODO
+    echo 'remote network performance'
+fi
 
-# echo 'local network performance'
 
-# echo 'remote network performance'
+
