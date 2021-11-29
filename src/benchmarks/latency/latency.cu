fa156f5d445151c10f65ca2e136d7fc8e5e04b4b
Lukas Tobler <lutobler@student.ethz.ch>
2021-11-29 11:18:23 +0100

    Update latency benchmark

--- src/benchmarks/latency/latency.cu
+++ src/benchmarks/latency/latency.cu
@@ -3,12 +3,13 @@
 
 #define N 4096
 
-__global__ void noop() {
-}
-
 int main() {
     auto s = std::chrono::high_resolution_clock::now();
-    noop<<<N, 1>>>();
+    float x = 1.0;
+    float *d_ptr;
+    cudaMalloc(&d_ptr, sizeof(float));
+    cudaMemcpy(d_ptr, &x, sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(&x, d_ptr, sizeof(float), cudaMemcpyDeviceToHost);
     auto e = std::chrono::high_resolution_clock::now();
     auto d = std::chrono::duration_cast<std::chrono::microseconds>(e-s).count() / 1000000.0;
     printf("%.8f\n", d);
