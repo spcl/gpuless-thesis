aa1f10ffa85e5ab6d5053300f82581a8575d20be
Lukas Tobler <lutobler@student.ethz.ch>
2021-12-15 16:57:46 +0100

    Add more cuda-traces

--- cuda-traces/analysis.sh
+++ cuda-traces/analysis.sh
@@ -7,6 +7,8 @@ files=(
     resnet50.log
     bert-squad.log
     3d-unet-kits19.log
+    srad_v1.log
+    bfs.log
 )
 
 printf '| Application | Kernel launches | Unique Kernels | cudaMalloc | Copy H2D | Copy D2D | Copy D2H |\n'
@@ -16,8 +18,10 @@ for f in ${files[@]}; do
     n_malloc=$(grep 'cudaMalloc' "$f" | wc -l)
     n_cpyd2h=$(grep 'DeviceToHost' "$f" | wc -l)
     n_cpyh2d=$(grep 'HostToDevice' "$f" | wc -l)
-    n_cpyd2d=$(grep 'DeviceToDevice' "$f" | wc -l)
+    # n_cpyd2d=$(grep 'DeviceToDevice' "$f" | wc -l)
     n_uniq_kernel=$(grep -E 'cuLaunchKernel' "$f" | sed 's/^\(cuLaunchKernel(.*\)\ \[.*$/\1/' | sort | uniq | wc -l)
 
-    printf "| $f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2d | $n_cpyd2h |\n"
+    # printf "| $f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2d | $n_cpyd2h |\n"
+    # printf "| $f | $n_kernel | $n_uniq_kernel | $n_malloc | $n_cpyh2d | $n_cpyd2h |\n"
+    printf "$f & $n_kernel & $n_uniq_kernel & $n_malloc & $n_cpyh2d & $n_cpyd2h &\n"
 done
