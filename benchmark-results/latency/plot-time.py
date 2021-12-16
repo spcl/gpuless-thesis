b322e16e69f34d6af8025f9c10364b050b09aba4
Lukas Tobler <lutobler@student.ethz.ch>
2021-12-16 09:45:08 +0100

    Update latency plot with preinit data

--- benchmark-results/latency/plot-time.py
+++ benchmark-results/latency/plot-time.py
@@ -12,7 +12,7 @@ for fname in os.listdir('data'):
     f = open('data/' + fname)
     lines = [l.rstrip() for l in f.readlines()]
 
-    r = r'benchmark-([a-zA-Z0-9_-]*)-(native|remote)-(remote|local)-.*'
+    r = r'benchmark-([a-zA-Z0-9_-]*)-(native|remote|preinit)-(remote|local)-.*'
     m = re.search(r, fname)
     benchmark_name = m.group(1)
     benchmark_type = m.group(2)
@@ -27,6 +27,9 @@ for fname in os.listdir('data'):
     elif benchmark_type == 'remote' and benchmark_flag == 'remote':
         for l in lines:
             v.append([benchmark_name, 'Remote TCP', float(l) * 1000.0])
+    elif benchmark_type == 'preinit' and benchmark_flag == 'local':
+        for l in lines:
+            v.append([benchmark_name, 'Native Pre-Initialized', float(l) * 1000.0])
 
     f.close()
 
@@ -37,7 +40,8 @@ sns.set(rc={"figure.figsize": (8.0, 5.5)})
 sns.set_theme(style='whitegrid')
 palette = sns.color_palette("muted")
 
-hue_order=['Native', 'Local TCP', 'Remote TCP']
+# hue_order=['Native', 'Native Pre-Initialized', 'Local TCP', 'Remote TCP']
+hue_order=['Native', 'Local TCP', 'Remote TCP', 'Native Pre-Initialized']
 ax = sns.barplot(x='time',
                  y='benchmark',
                  hue='type',
@@ -49,7 +53,7 @@ ax = sns.barplot(x='time',
 # ax.set_ylabel('Benchmark', rotation='horizontal')
 ax.set_ylabel('')
 ax.set_xlabel('Time [ms]')
-# ax.set_xscale('log')
+ax.set_xscale('log')
 ax.yaxis.set_label_coords(-0.06, 1.02)
 ax.legend(loc='lower right')
 ax.set_title('Latency of minimal CUDA API execution\nNVIDIA A100, n=100, 95% confidence')
