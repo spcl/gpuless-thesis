1c9c0750df6c455f0da78ea96212382fb7da7199
Lukas Tobler <lutobler@student.ethz.ch>
2021-12-28 14:16:28 +0100

    Add yolop benchmark

---
+++ src/benchmarks/yolop/run.py
@@ -0,0 +1,16 @@
+import torch
+from timeit import default_timer as timer
+
+start = timer()
+
+# load model
+model = torch.hub.load('hustvl/yolop', 'yolop', pretrained=True)
+
+#inference
+img = torch.randn(1,3,640,640)
+det_out,da_seg_out,ll_seg_out = model(img)
+
+# print(da_seg_out[0][0])
+
+end = timer()
+print(end - start)
