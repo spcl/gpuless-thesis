1400be2ba5d6d1fa3c37a84373a5be6b70de269f
Lukas Tobler <lutobler@student.ethz.ch>
2021-12-30 15:51:03 +0100

    Add batched execution to midas benchmark

---
+++ src/benchmarks/midas/run-batched.py
@@ -0,0 +1,55 @@
+import cv2
+import torch
+import urllib.request
+import matplotlib.pyplot as plt
+
+from timeit import default_timer as timer
+import time
+
+model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+#model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+#model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+midas.eval()
+
+device = torch.device("cuda")
+midas.to(device)
+
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+
+img = cv2.imread('dog.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+def inference():
+    input_batch = transform(img).to(device)
+    with torch.no_grad():
+        prediction = midas(input_batch)
+        prediction = torch.nn.functional.interpolate(
+            prediction.unsqueeze(1),
+            size=img.shape[:2],
+            mode="bicubic",
+            align_corners=False,
+        ).squeeze()
+
+    output = prediction.cpu().numpy()
+    # plt.imshow(output)
+    # plt.show()
+    # plt.savefig('out.pdf')
+
+# warmup
+for i in range(0, 5):
+    inference()
+
+# benchmark
+for i in range(0, 100):
+    start = timer()
+    inference()
+    end = timer()
+    print(end - start)
+    time.sleep(0.2)
