9aca9e77f16f6d3bc26d9aa68f3cb1ab04db21d5
Paul Fischill <paul@fischill.org>
2022-10-30 16:43:07 +0100

    Obtain real dlsym in a way working with current glibc version

--- src/trace/dlsym_util.cpp
+++ src/trace/dlsym_util.cpp
@@ -1,8 +1,9 @@
 #include "dlsym_util.hpp"
 #include <dlfcn.h>
+#include <spdlog/cfg/env.h>
+#include <spdlog/spdlog.h>
 
 void *real_dlsym(void *handle, const char *symbol) {
-    static auto internal_dlsym = (decltype(&dlsym))__libc_dlsym(
-        __libc_dlopen_mode("libdl.so.2", RTLD_LAZY), "dlsym");
+    static auto internal_dlsym = (decltype(&dlsym))dlvsym(RTLD_NEXT, "dlsym", "GLIBC_2.34");
     return (*internal_dlsym)(handle, symbol);
 }
