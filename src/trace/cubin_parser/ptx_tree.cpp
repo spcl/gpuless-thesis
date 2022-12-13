e133e0c14e7a43d719d8b04f2fdcaabee4db7664
Jonas Bachmann <jbachmann@ethz.ch>
2022-12-13 16:30:04 +0100

    resolved multiple cases in op to str

--- src/trace/cubin_parser/ptx_tree.cpp
+++ src/trace/cubin_parser/ptx_tree.cpp
@@ -826,42 +826,17 @@ std::string stringFromNodeKind(PtxNodeKind kind) {
         return "SadOp";
     case PtxNodeKind::LdOp:
         return "LdOp";
+    case PtxNodeKind::AbdOp:
+        return "AbdOp";
     case PtxNodeKind::MoveOp:
         return "MoveOp";
     case PtxNodeKind::Register:
         return "Register";
     case PtxNodeKind::invalidOp:
         return "InvalidOp";
-    case PtxNodeKind::SubOp:
-        return "SubOp";
-    case PtxNodeKind::MulOp:
-        return "MulOp";
-    case PtxNodeKind::AbdOp:
-        return "AbdOp";
-    case PtxNodeKind::MadOp:
-        return "MadOp";
-    case PtxNodeKind::SadOp:
-        return "SadOp";
-    case PtxNodeKind::DivOp:
-        return "DivOp";
-    case PtxNodeKind::RemOp:
-        return "RemOp";
-    case PtxNodeKind::AbsOp:
-        return "AbsOp";
-    case PtxNodeKind::NegOp:
-        return "NegOp";
-    case PtxNodeKind::MinOp:
-        return "MinOp";
-    case PtxNodeKind::MaxOp:
-        return "MaxOp";
-    case PtxNodeKind::ShlOp:
-        return "ShlOp";
-    case PtxNodeKind::ShrOp:
-        return "ShrOp";
-    case PtxNodeKind::LdOp:
-        return "LdOp";
+
     }
-}
+    }
 
 std::string stringFromSpecialRegister(SpecialRegisterKind kind) {
     switch (kind) {
