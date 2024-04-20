dfd8b8a4e5aff2e8629893a14c0826fd5fb9b244
EricPyZhou <ericzpy1998@outlook.com>
2024-04-20 06:20:36 +0200

    local API remoting script

---
+++ src/benchmarks/benchmark-api-remoting-scientific.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+n_runs=15
+
+# command line arguments
+project_dir="$1"
+bench_dir="$2"
+bench_type="$3"
+cuda_bin="$4"
+remote_ip="$5"
+note="$6"
+
+bechmark_name=$(basename "$bench_dir")
+out_file="${project_dir}/benchmark-results/trace-execution/benchmark-${bechmark_name}-${bench_type}-${note}-$(date --iso-8601=seconds)"
+manager_bin=$project_dir/src/build/manager_trace
+
+run_bench_remote() {
+    echo 'local network performance'
+
+    pushd .
+    cd $bench_dir
+    printf '' > "$out_file" # clear output file
+
+    for ((i=0; i<$n_runs; i++)); do
+        t=$(LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64:$CUDA_HOME/extras/CUPTI/lib64 CUDA_VISIBLE_DEVICES=0 SPDLOG_LEVEL=OFF MANAGER_IP=${remote_ip} MANAGER_PORT=8002 CUDA_BINARY=${cuda_bin} EXECUTOR_TYPE=tcp LD_PRELOAD=${project_dir}/src/build_trace/libgpuless.so ./run_timed.sh)
+        printf "$t\n" >> "$out_file"
+        echo "run ${i}: ${t}"
+        sleep 1.0
+    done
+
+    popd
+    echo ''
+}
