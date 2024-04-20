d2ba58ac4e59a2f8c20dd77fd84dd07ef1b16af7
Marcin Copik <mcopik@gmail.com>
2024-04-20 23:16:38 +0200

    move directory

--- src/benchmarks/benchmark-api-remoting-scientific.sh
+++ src/benchmarks/benchmark-api-remoting-scientific.sh
@@ -11,7 +11,7 @@ remote_ip="$5"
 note="$6"
 
 bechmark_name=$(basename "$bench_dir")
-out_file="${project_dir}/benchmark-results/trace-execution/benchmark-${bechmark_name}-${bench_type}-${note}-$(date --iso-8601=seconds)"
+out_file="${project_dir}/benchmark-results/cuda-analysis/benchmark-${bechmark_name}-${bench_type}-${note}-$(date --iso-8601=seconds)"
 manager_bin=$project_dir/src/build/manager_trace
 
 run_bench_native() {
